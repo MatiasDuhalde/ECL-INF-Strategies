b9eee91286cb3f78cbcd278684a43154671dcf41
Matías Duhalde <matias.duhalde@uc.cl>
2022-09-19 14:40:17 +0200

    feat: tweak labyrinth algorithm

--- BE/labyrinthe/labyrinthe_v2.py
+++ BE/labyrinthe/labyrinthe_v2.py
@@ -1,6 +1,7 @@
 """
 Résoudre le problème du labyrinthe
 """
+from time import time
 from remplisage import Labyrinthe
 
 # — libre : -1
@@ -13,57 +14,55 @@ from remplisage import Labyrinthe
 TAB_DELTA_X_Y = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
 
-def numerotation_des_cases(mat_des_numeros, G, noeud_courant, numero_de_cette_case):
-    x_courant = noeud_courant[0]
-    y_courant = noeud_courant[1]
-    if G[x_courant][y_courant] == 3:
+def initialiser_matrice_des_numeros(mat):
+    mat = []
+    for i, row in enumerate(mat):
+        mat.append([])
+        for el in row:
+            if el == 0:
+                mat[i].append(10000000000)
+            else:
+                mat[i].append(el)
+    return mat
+
+
+def numerotation_des_cases(mat_des_numeros, labyrinthe, noeud_courant, numero_de_cette_case):
+    x_courant, y_courant = noeud_courant
+    if labyrinthe[x_courant][y_courant] == 3:
         return True
-    for offset in TAB_DELTA_X_Y:
-        next_x = x_courant + offset[0]
-        next_y = y_courant + offset[1]
-        if prometteur(G, (next_x, next_y)):
-            if G[next_x][next_y] == 2 or G[next_x][next_y] == 3:
+    for step in TAB_DELTA_X_Y:
+        next_x = x_courant + step[0]
+        next_y = y_courant + step[1]
+        if prometteur(labyrinthe, (next_x, next_y)):
+            if labyrinthe[next_x][next_y] == 2 or labyrinthe[next_x][next_y] == 3:
                 continue
             if mat_des_numeros[next_x][next_y] < numero_de_cette_case:
                 continue
             mat_des_numeros[next_x][next_y] = min(
                 mat_des_numeros[next_x][next_y], numero_de_cette_case + 1)
-            numerotation_des_cases(mat_des_numeros, G, (next_x, next_y),
+            numerotation_des_cases(mat_des_numeros, labyrinthe, (next_x, next_y),
                                    mat_des_numeros[next_x][next_y])
 
 
-def trouver_depart(G):
-    for i in range(len(G)):
-        for j in range(len(G[i])):
-            if G[i][j] == 2:
+def trouver_depart(matrice):
+    for i, row in enumerate(matrice):
+        for j, el in enumerate(row):
+            if el == 2:
                 return i, j
     return None
 
 
-def prometteur(G, noeud):
-    x = noeud[0]
-    y = noeud[1]
-    return 0 <= x < len(G) and 0 <= y < len(G[x]) and G[x][y] != 1
+def prometteur(mat, noeud):
+    x, y = noeud
+    return 0 <= x < len(mat) and 0 <= y < len(mat[x]) and mat[x][y] != 1
 
 
-def print_G(G):
-    for i in range(len(G)):
-        for j in range(len(G[i])):
-            print(G[i][j], end=' ')
+def print_G(matrice):
+    for row in matrice:
+        for el in row:
+            print(el, end=' ')
         print()
     print()
-
-
-def initialiser_matrice_des_numeros(G):
-    mat = []
-    for i in range(len(G)):
-        mat.append([])
-        for j in range(len(G[i])):
-            if G[i][j] == 0:
-                mat[i].append(10000000000)
-            else:
-                mat[i].append(G[i][j])
-    return mat
 
 
 if __name__ == '__main__':
@@ -72,9 +71,15 @@ if __name__ == '__main__':
     offset = 100
     G = lab.obtenir_matrice()
     depart = trouver_depart(G)
-    mat_des_numeros = initialiser_matrice_des_numeros(G)
-    print(mat_des_numeros)
+    matrice_des_numeros = initialiser_matrice_des_numeros(G)
+    print(matrice_des_numeros)
     print(depart)
     print_G(G)
-    numerotation_des_cases(mat_des_numeros, G, depart, offset)
-    print_G(mat_des_numeros)
+
+    t_1 = time()
+    numerotation_des_cases(matrice_des_numeros, G, depart, offset)
+    t_2 = time()
+
+    print_G(matrice_des_numeros)
+
+    print('Temps de calcul :', t_2 - t_1)
