8b1a1abd889ed12a0fa3284051c85427821a6611
Matias Duhalde <matias.duhalde@uc.cl>
2022-09-25 17:30:03 +0200

    refactor: use classes for the cavaliers problem

--- BE/cavaliers/cavaliers_v1.py
+++ BE/cavaliers/cavaliers_v1.py
@@ -4,37 +4,108 @@ Résoudre le problème des cavaliers
 Algorithme à Essais Successifs (AES), implementation naïve, avec backtracking simple
 """
 from time import time
-import numpy as np
-
-TAB_DELTA_X_Y = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
+from utils import Case, creer_matrice
 
 
-def creer_matrice(dim):
-    """Créer une matrice carrée de taille n"""
-    return -1 * np.ones((dim, dim))
+class Echiquier:
+    """Échiquier Classe pour résoudre le problème du cavalier
+    """
 
+    TAB_DELTA_X_Y = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
 
-def AES_parcours_cavalier_un_succes_suffit(echiquier, taille, derniere_case_traitee,
-                                           prochain_num_etape, compteurs):
-    compteurs[0] += 1
-    if prochain_num_etape > taille * taille:
-        return True
-    for i in range(8):
-        next_x = derniere_case_traitee[0] + TAB_DELTA_X_Y[i][0]
-        next_y = derniere_case_traitee[1] + TAB_DELTA_X_Y[i][1]
-        if prometteur(echiquier, taille, next_x, next_y):
-            echiquier[next_x][next_y] = prochain_num_etape
-            res = AES_parcours_cavalier_un_succes_suffit(
-                echiquier, taille, (next_x, next_y), prochain_num_etape + 1, compteurs)
-            if res:
-                return True
-            echiquier[next_x][next_y] = -1
-            compteurs[1] += 1
-    return False
+    def __init__(self, dimension: int, case_de_depart: Case):
+        self.dimension = dimension
+        self.nombre_de_cases = dimension * dimension
+        self.case_de_depart = case_de_depart
+        self.reset()
 
+    def reset(self) -> None:
+        """Remettre l'échiquier dans son état initial
+        """
+        self.__matrice = creer_matrice(self.dimension)
+        self.fixer_case(self.case_de_depart, 1)
+        self.backtracks = 0
+        self.tentatives = 0
 
-def prometteur(echiquier, taille, next_x, next_y):
-    return 0 <= next_x < taille and 0 <= next_y < taille and echiquier[next_x][next_y] == -1
+    def prometteur(self, case: Case) -> bool:
+        """Vérifier si case est valide pour le cavalier
+
+        Args:
+            case (Case): case tentative pour y aller
+
+        Returns:
+            bool: True si la case est valide, sinon False
+        """
+        _x, _y = case
+        return 0 <= _x < self.dimension and 0 <= _y < self.dimension and not self.is_taken(case)
+
+    def is_taken(self, case: Case) -> bool:
+        """Vérifier si la case est déjà occupée
+
+        Args:
+            case (Case): case tentative à vérifier
+
+        Returns:
+            bool: True si la case est déjà occupée, sinon False
+        """
+        _x, _y = case
+        return self.__matrice[_x, _y] != -1
+
+    def liberer_case(self, case: Case) -> None:
+        """Libérer la case désirée (la remettre dans son état initial, sauf case de depart)
+
+        Args:
+            case (Case): case à libérer
+        """
+        self.fixer_case(case, -1)
+
+    def fixer_case(self, case: Case, valeur: int) -> None:
+        """Fixer la valeur de la case désirée
+
+        Args:
+            case (Case): case désirée
+            valeur (int): valeur à fixer
+        """
+        _x, _y = case
+        self.__matrice[_x, _y] = valeur
+
+    def aes_parcour_cavalier_un_succes_suffit(self, derniere_case_traitee: Case,
+                                              prochain_num_etape: int) -> bool:
+        """Partie récursive (AES) du problème du cavalier
+
+        Args:
+            derniere_case_traitee (Case): Case où le cavalier se trouve maintenant
+            prochain_num_etape (int): étape actuelle du parcours
+
+        Returns:
+            bool: False si l'algorithme ne trouve pas une solution pour l'état actuel, sinon True
+        """
+        x_actuel, y_actuel = derniere_case_traitee
+        self.tentatives += 1
+        if prochain_num_etape > self.nombre_de_cases:
+            return True
+        for x_offset, y_offset in Echiquier.TAB_DELTA_X_Y:
+            nouvelle_case = (x_actuel + x_offset, y_actuel + y_offset)
+            if self.prometteur(nouvelle_case):
+                self.fixer_case(nouvelle_case, prochain_num_etape)
+                res = self.aes_parcour_cavalier_un_succes_suffit(
+                    nouvelle_case, prochain_num_etape + 1)
+                if res:
+                    return True
+                self.liberer_case(nouvelle_case)
+                self.backtracks += 1
+        return False
+
+    def resoudre(self) -> bool:
+        """Résoudre le problème du cavalier
+
+        Returns:
+            bool: _description_
+        """
+        return self.aes_parcour_cavalier_un_succes_suffit(self.case_de_depart, 2)
+
+    def __repr__(self) -> str:
+        return str(self.__matrice)
 
 
 if __name__ == '__main__':
@@ -42,22 +113,18 @@ if __name__ == '__main__':
     depart = tuple(
         map(int, (input(f"Départ du cavalier ? (0<=x,y<={N-1}; défaut 0 0) ") or "0 0").split()))
 
-    G = creer_matrice(N)
-    G[depart[0], depart[1]] = 1
-
-    prochaine_numero = 2
-
-    stats = [0, 0]
+    echiquier = Echiquier(N, depart)
 
     t_1 = time()
-    final_res = AES_parcours_cavalier_un_succes_suffit(G, N, depart, prochaine_numero, stats)
+    resultat_final = echiquier.resoudre()
     t_2 = time()
 
-    if final_res:
-        print(G)
+    if resultat_final:
+        print('Solution trouvée:')
+        print(echiquier)
     else:
-        print('échec')
+        print('Échec')
 
-    print('Nombre de tentatives :', stats[0])
-    print('Nombre de backtracks :', stats[1])
-    print('Temps de calcul :', t_2 - t_1)
+    print('Nombre de tentatives :', echiquier.tentatives)
+    print('Nombre de backtracks :', echiquier.backtracks)
+    print(f'Temps de calcul : {t_2 - t_1}s')
---
+++ BE/cavaliers/utils.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+
+Case = tuple[int, int]
+
+
+def creer_matrice(dim: int) -> np.ndarray:
+    """Créer une matrice carrée de taille dim remplie de -1
+
+    Args:
+        dim (int): dimension de la matrice à créer
+
+    Returns:
+        np.ndarray: matrice résultant
+    """
+    return -1 * np.ones((dim, dim))
+
+
+def creer_matrice_avec_bandes(dim: int) -> np.ndarray:
+    """Créer une matrice avec bandes carrée de taille dim remplie de -1
+
+    Args:
+        dim (int): dimension de la matrice à créer (sans compter les bandes)
+
+    Returns:
+        np.ndarray: matrice résultant
+    """
+    m_base = np.zeros((dim + 4, dim + 4))
+    for i in range(2, dim + 2):
+        for j in range(2, dim + 2):
+            m_base[i][j] = -1
+    return m_base
