722227ef974e36fe99614518d7e6dbb84f5b4bf7
Matias Duhalde <matias.duhalde@uc.cl>
2022-09-25 20:55:08 +0200

    refactor: cavaliers v3

--- BE/cavaliers/cavaliers_v3.py
+++ BE/cavaliers/cavaliers_v3.py
@@ -1,138 +1,197 @@
 """
 Résoudre le problème des cavaliers
 
-Algorithme à Essais Successifs (AES) utilisant matrice de voisins
-Code basé sur des slides du cours et annexes
+Algorithme à Essais Successifs (AES), implementation naïve, avec bande
 """
 from time import time
+
 import numpy as np
-
-TAB_DELTA_X_Y = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
-
-
-def creer_matrice(dim):
-    """Créer une matrice carrée de taille n (avec des bandes)"""
-    m_base = np.zeros((dim + 4, dim + 4))
-    for i in range(2, dim + 2):
-        for j in range(2, dim + 2):
-            m_base[i][j] = -1
-    return m_base
+from utils import Case, creer_matrice_avec_bandes
 
 
-def calculer_voisins(matrice, dim):
-    voisins = np.zeros((dim, dim))
-    for i in range(dim):
-        for j in range(dim):
-            for k in range(8):
-                next_x = i + TAB_DELTA_X_Y[k][0] + 2
-                next_y = j + TAB_DELTA_X_Y[k][1] + 2
-                if prometteur(matrice, next_x, next_y):
-                    voisins[i][j] += 1
-    return voisins
+class Echiquier:
+    """Échiquier Classe pour résoudre le problème du cavalier
+    """
 
+    TAB_DELTA_X_Y = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
 
-def mise_a_jour_voisins(matrice, voisins, pos):
-    x, y = pos
-    for k in range(8):
-        next_x, next_y = x + TAB_DELTA_X_Y[k][0], y + TAB_DELTA_X_Y[k][1]
-        if not prometteur(matrice, next_x, next_y):
-            continue
-        voisins[next_x - 2][next_y - 2] -= 1
+    def __init__(self, dimension: int, case_de_depart: Case):
+        self.dimension = dimension
+        self.nombre_de_cases = dimension * dimension
+        self.case_de_depart = (case_de_depart[0] + 2, case_de_depart[1] + 2)
+        self.reset()
 
+    def reset(self) -> None:
+        """Remettre l'échiquier dans son état initial
+        """
+        self.__matrice = creer_matrice_avec_bandes(self.dimension)
+        self.fixer_case(self.case_de_depart, 1)
+        self.__voisins = self.obtenir_matrice_de_voisins()
+        self.backtracks = 0
+        self.tentatives = 0
 
-def remise_a_jour_voisins(matrice, voisins, pos):
-    x, y = pos
-    for k in range(8):
-        next_x, next_y = x + TAB_DELTA_X_Y[k][0], y + TAB_DELTA_X_Y[k][1]
-        if not prometteur(matrice, next_x, next_y):
-            continue
-        voisins[next_x - 2][next_y - 2] += 1
+    def obtenir_matrice_de_voisins(self) -> np.ndarray:
+        """Obtenir la matrice de voisins de l'échiquier
 
+        Returns:
+            np.ndarray: Matrice de voisins calculée
+        """
+        voisins = np.zeros((self.dimension, self.dimension))
+        for i in range(self.dimension):
+            for j in range(self.dimension):
+                for x_offset, y_offset in Echiquier.TAB_DELTA_X_Y:
+                    # echiquer a une bande donc il faut ajouter 2
+                    x_suivant = i + x_offset + 2
+                    y_suivant = j + y_offset + 2
+                    if self.prometteur((x_suivant, y_suivant)):
+                        voisins[i, j] += 1
+        return voisins
 
-def trouver_meilleurs_voisins_libres(matrice, matrice_voisins, pos_cavalier):
-    min_k = trouver_le_premier_voisin_de_degre_minimal_libre(matrice, matrice_voisins, pos_cavalier)
-    if min_k >= 0:
-        return trouver_tous_les_best_voisins(matrice, matrice_voisins, pos_cavalier, min_k)
-    return []
+    def mise_a_jour_voisins(self, position_actuel: Case, inverse=False) -> None:
+        """Mise à jour la matrice de voisins quand la position du cavalier change
 
+        Args:
+            position_actuel (Case): position du cavalier
+            inverse (bool, optional): Si c'est False, la fonction réduira la quantité de voisins.
+                Si c'est True, la fonction augmentera la quantité de voisins. Defaults to False.
+        """
+        _x, _y = position_actuel
+        ajout = 1 if inverse else -1
+        for x_offset, y_offset in Echiquier.TAB_DELTA_X_Y:
+            x_suivant, y_suivant = _x + x_offset, _y + y_offset
+            if self.prometteur((x_suivant, y_suivant)):
+                # matrice de voisins n'a pas de bande donc il faut soustraire 2
+                self.__voisins[x_suivant - 2][y_suivant - 2] += ajout
 
-def trouver_le_premier_voisin_de_degre_minimal_libre(matrice, matrice_voisins, pos_cavalier):
-    x, y = pos_cavalier
-    min_k = 9
-    # nx_k, ny_k = -1, -1
-    for k in range(8):
-        nx, ny = x + TAB_DELTA_X_Y[k][0], y + TAB_DELTA_X_Y[k][1]
-        if not prometteur(matrice, nx, ny):
-            continue
-        if min_k > matrice_voisins[nx - 2][ny - 2]:
-            min_k = matrice_voisins[nx - 2][ny - 2]
-            # nx_k, ny_k = nx, ny
-    return min_k
+    def trouver_meilleurs_voisins_libres(self, position_actuel: Case) -> list[Case]:
+        k_min = self.trouver_le_premier_voisin_de_degre_minimal_libre(position_actuel)
+        if k_min >= 0:
+            return self.trouver_tous_les_meilleurs_voisins(position_actuel, k_min)
+        # Il n'y a pas de voisins
+        return []
 
+    def trouver_le_premier_voisin_de_degre_minimal_libre(self, position_actuel: Case) -> int:
+        _x, _y = position_actuel
+        # on commence par le maximum (9)
+        k_min = 9
+        for x_offset, y_offset in Echiquier.TAB_DELTA_X_Y:
+            x_suivant, y_suivant = _x + x_offset, _y + y_offset
+            if self.prometteur((x_suivant, y_suivant)):
+                # matrice de voisins n'a pas de bande donc il faut soustraire 2
+                if k_min > self.__voisins[x_suivant - 2][y_suivant - 2]:
+                    k_min = self.__voisins[x_suivant - 2][y_suivant - 2]
+        return k_min
 
-def trouver_tous_les_best_voisins(matrice, matrice_voisins, pos_cavalier, min_k):
-    best_voisins = []
-    x, y = pos_cavalier
-    for z in range(8):
-        nx, ny = x + TAB_DELTA_X_Y[z][0], y + TAB_DELTA_X_Y[z][1]
-        if not prometteur(matrice, nx, ny):
-            continue
-        if min_k == matrice_voisins[nx - 2][ny - 2]:
-            best_voisins.append((nx, ny))
-    return best_voisins
+    def trouver_tous_les_meilleurs_voisins(self, position_actuel: Case, k_min: int) -> list[Case]:
+        meilleurs_voisins = []
+        _x, _y = position_actuel
+        for x_offset, y_offset in Echiquier.TAB_DELTA_X_Y:
+            x_suivant, y_suivant = _x + x_offset, _y + y_offset
+            if self.prometteur((x_suivant, y_suivant)):
+                # matrice de voisins n'a pas de bande donc il faut soustraire 2
+                if k_min == self.__voisins[x_suivant - 2][y_suivant - 2]:
+                    meilleurs_voisins.append((x_suivant, y_suivant))
+        return meilleurs_voisins
 
+    def prometteur(self, case: Case) -> bool:
+        """Vérifier si case est valide pour le cavalier
 
-def AES_parcours_cavalier_un_succes_suffit(echiquier, taille, voisins, derniere_case_traitee,
-                                           prochain_num_etape, compteurs):
-    compteurs[0] += 1
-    if prochain_num_etape > taille * taille:
-        return True
-    best_voisins = trouver_meilleurs_voisins_libres(echiquier, voisins, derniere_case_traitee)
-    for best_voisin in best_voisins:
-        next_x, next_y = best_voisin[0], best_voisin[1]
-        mise_a_jour_voisins(echiquier, voisins, best_voisin)
-        if prometteur(echiquier, next_x, next_y):
-            echiquier[next_x][next_y] = prochain_num_etape
-            res = AES_parcours_cavalier_un_succes_suffit(
-                echiquier, taille, voisins, (next_x, next_y), prochain_num_etape + 1, compteurs)
-            if res:
-                return True
-            echiquier[next_x][next_y] = -1
-            remise_a_jour_voisins(echiquier, voisins, best_voisin)
-            compteurs[1] += 1
-    return False
+        Args:
+            case (Case): case tentative pour y aller
 
+        Returns:
+            bool: True si la case est valide, sinon False
+        """
+        return not self.is_taken(case)
 
-def prometteur(echiquier, next_x, next_y):
-    return echiquier[next_x][next_y] == -1
+    def is_taken(self, case: Case) -> bool:
+        """Vérifier si la case est déjà occupée
+
+        Args:
+            case (Case): case tentative à vérifier
+
+        Returns:
+            bool: True si la case est déjà occupée, sinon False
+        """
+        _x, _y = case
+        return self.__matrice[_x, _y] != -1
+
+    def liberer_case(self, case: Case) -> None:
+        """Libérer la case désirée (la remettre dans son état initial, sauf case de depart)
+
+        Args:
+            case (Case): case à libérer
+        """
+        self.fixer_case(case, -1)
+
+    def fixer_case(self, case: Case, valeur: int) -> None:
+        """Fixer la valeur de la case désirée
+
+        Args:
+            case (Case): case désirée
+            valeur (int): valeur à fixer
+        """
+        _x, _y = case
+        self.__matrice[_x, _y] = valeur
+
+    def aes_parcour_cavalier_un_succes_suffit(self, derniere_case_traitee: Case,
+                                              prochain_num_etape: int) -> bool:
+        """Partie récursive (AES) du problème du cavalier
+
+        Args:
+            derniere_case_traitee (Case): Case où le cavalier se trouve maintenant
+            prochain_num_etape (int): étape actuelle du parcours
+
+        Returns:
+            bool: False si l'algorithme ne trouve pas une solution pour l'état actuel, sinon True
+        """
+        self.tentatives += 1
+        if prochain_num_etape > self.nombre_de_cases:
+            return True
+        meilleurs_voisins = self.trouver_meilleurs_voisins_libres(derniere_case_traitee)
+        print(len(meilleurs_voisins))
+        for nouvelle_case in meilleurs_voisins:
+            self.mise_a_jour_voisins(nouvelle_case)
+            if self.prometteur(nouvelle_case):
+                self.fixer_case(nouvelle_case, prochain_num_etape)
+                res = self.aes_parcour_cavalier_un_succes_suffit(
+                    nouvelle_case, prochain_num_etape + 1)
+                if res:
+                    return True
+                self.liberer_case(nouvelle_case)
+                self.mise_a_jour_voisins(nouvelle_case, True)
+                self.backtracks += 1
+        return False
+
+    def resoudre(self) -> bool:
+        """Résoudre le problème du cavalier
+
+        Returns:
+            bool: _description_
+        """
+        return self.aes_parcour_cavalier_un_succes_suffit(self.case_de_depart, 2)
+
+    def __repr__(self) -> str:
+        return str(self.__matrice[2:2+self.dimension, 2:2+self.dimension])
 
 
 if __name__ == '__main__':
     N = int(input("Taille de la matrice ? (>4; défaut 5) ") or 5)
     depart = tuple(
         map(int, (input(f"Départ du cavalier ? (0<=x,y<={N-1}; défaut 0 0) ") or "0 0").split()))
-    depart_reel = (depart[0] + 2, depart[1] + 2)
 
-    print("Taille : ", N)
-    print("Départ : ", depart)
-
-    G = creer_matrice(N)
-    V = calculer_voisins(G, N)
-    G[depart_reel[0], depart_reel[1]] = 1
-
-    prochaine_numero = 2
-
-    stats = [0, 0]
+    echiquier = Echiquier(N, depart)
 
     t_1 = time()
-    final_res = AES_parcours_cavalier_un_succes_suffit(
-        G, N, V, depart_reel, prochaine_numero, stats)
+    resultat_final = echiquier.resoudre()
     t_2 = time()
-    if final_res:
-        for row in range(2, N + 2):
-            print(G[2:N + 2, row])
+
+    if resultat_final:
+        print('Solution trouvée:')
+        print(echiquier)
     else:
-        print('échec')
-    print('Nombre de tentatives :', stats[0])
-    print('Nombre de backtracks :', stats[1])
-    print('Temps de calcul :', t_2 - t_1)
+        print('Échec')
+
+    print('Nombre de tentatives :', echiquier.tentatives)
+    print('Nombre de backtracks :', echiquier.backtracks)
+    print(f'Temps de calcul : {t_2 - t_1}s')
