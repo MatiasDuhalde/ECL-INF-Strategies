2b5bdf2479947ef2289de217e0c1c3ac58f17c8f
Matías Duhalde <matias.duhalde@uc.cl>
2022-10-10 15:12:11 +0200

    feat: morpion interface

--- BE/morpion/interface.py
+++ BE/morpion/interface.py
@@ -96,9 +96,20 @@ class Interface(tk.Tk):
 
     def action_reinitialiser(self):
         print('Réinitialisation...')
+        self.morpion = Morpion(self.dimension, interface=self)
+        self.retracer()
 
     def action_quitter(self):
         self.destroy()
+
+    def retracer(self):
+        for i in range(self.dimension):
+            for j in range(self.dimension):
+                case = (i, j)
+                self.effacer(case)
+                forme = self.morpion.get_case_value(case)
+                if forme is not None:
+                    self.tracer(forme, case)
 
     def tracer(self, forme: str, case: Case):
         # Trace la forme dans la case, rond ou croix
@@ -108,8 +119,7 @@ class Interface(tk.Tk):
         else:
             self.canvas.create_line(*(case_coords))
             _a, _b, _c, _d = case_coords
-            print(case_coords)
-            self.canvas.create_line(_c, _d, _b, _d)
+            self.canvas.create_line(_a, _d, _c, _b)
         self.update()
 
     def effacer(self, case):
@@ -133,51 +143,63 @@ class Morpion():
     def __init__(self, dimension: int, interface=None):
         self.dimension = dimension
         self.interface: Union[None, Interface] = interface
+        self.cases_per_joueur = self.dimension
         self.matrice: list[list[Union[str, None]]] = [
             [None for j in range(self.dimension)] for i in range(self.dimension)
         ]
         self.joueur_actuel = random.choice(self.JOEURS)
         self.nombre_tour = 0
+        self.coords_joueur = {joueur: [] for joueur in self.JOEURS}
         self.case_a_vider: Union[None, tuple[int, int]] = None
         self.vainqueur: Union[None, str] = None
         self.ia = None
 
-    # def essai_marquer_case(self, case):
-    #     _x, _y = case
-    #     if self.matrice[_x][_y] is None:
-    #         self.matrice[_x][_y] = random.choice(['rond', 'croix'])
-    #         self.repaint()
-    #     else:
-    #         print(f"La case {case} n'est pas vide !")
-    #         self.case_a_vider = case
-    #         self.matrice[self.case_a_vider] = None
-    #         self.repaint()
-
-    def essai_marquer_case(self, case: Case):
-        _x, _y = case
-        if self.matrice[_x][_y] is None:
-            self.matrice[_x][_y] = self.joueur_actuel
+    def essai_marquer_case(self, case: Case) -> Union[bool, tuple[Case, Union[str, None]]]:
+        if (self.case_est_libre(case) and
+                self.joueur_peut_ajouter_nouvelle_forme(self.joueur_actuel) and
+                self.case_a_vider != case):
+            self.set_case_value(case, self.joueur_actuel)
             if self.interface:
                 self.interface.tracer(self.joueur_actuel, case)
-            self.basculer_joeur()
             self.case_a_vider = None
-        elif not self.case_a_vider and self.matrice[_x][_y] == self.joueur_actuel:
-            self.matrice[_x][_y] = None
+            self.basculer_joeur()
+            return case, self.joueur_actuel
+        if (self.case_a_vider is None and
+            not self.joueur_peut_ajouter_nouvelle_forme(self.joueur_actuel) and
+                self.case_est_de_joeueur(case, self.joueur_actuel)):
+            self.set_case_value(case, None)
             if self.interface:
                 self.interface.effacer(case)
             self.case_a_vider = case
+            return case, None
+        return False
+
+    def joueur_peut_ajouter_nouvelle_forme(self, joueur: str) -> bool:
+        return self.get_nombre_cases_occupees_du_joueur(joueur) < self.cases_per_joueur
+
+    def get_nombre_cases_occupees_du_joueur(self, joueur: str) -> int:
+        return len(self.coords_joueur[joueur])
+
+    def case_est_libre(self, case: Case) -> bool:
+        return self.get_case_value(case) is None
+
+    def case_est_de_joeueur(self, case: Case, joueur: str) -> bool:
+        return self.get_case_value(case) == joueur
+
+    def get_case_value(self, case: Case) -> Union[str, None]:
+        _x, _y = case
+        return self.matrice[_x][_y]
+
+    def set_case_value(self, case: Case, value: Union[str, None]) -> Union[str, None]:
+        _x, _y = case
+        self.matrice[_x][_y] = value
+        if value is not None:
+            self.coords_joueur[value].append(case)
+        else:
+            self.coords_joueur[self.joueur_actuel].remove(case)
 
     def basculer_joeur(self):
         self.joueur_actuel = 'rond' if self.joueur_actuel == 'croix' else 'croix'
-
-    def repaint(self):
-        if self.interface:
-            for i in range(self.dimension):
-                self.interface.effacer(i)
-                if self.matrice[i] == 'croix':
-                    self.interface.tracer('croix', i)
-                elif self.matrice[i] == 'rond':
-                    self.interface.tracer('rond', i)
 
 
 def print_matrice(M: list):
