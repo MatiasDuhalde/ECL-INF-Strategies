d81fa3888f515cbcf17ffdfa16a2e52d422b7c55
Matias Duhalde <matias.duhalde@uc.cl>
2022-11-04 23:18:55 +0100

    feat: working version

--- BE/morpion/morpion_v4.py
+++ BE/morpion/morpion_v4.py
@@ -1,13 +1,15 @@
-
+"""Travaux à rendre 4"""
 from __future__ import annotations
 
+import math
 import sys
 from collections import defaultdict
+from copy import deepcopy
 from random import choice
 from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
-    from utils import Case
+    from utils import Case, Plateau
 
 
 class Morpion():
@@ -33,7 +35,7 @@ class Morpion():
         self.vainqueur: Union[None, str]
         self.etats_precedents: list[tuple[set[Case], set[Case]]]
         self.res_stack = []
-        self.min_max_profondeur = 3
+        self.min_max_profondeur = 2
 
         self.reinitialiser()
 
@@ -112,17 +114,19 @@ class Morpion():
                 return case_choisi
         return None
 
-    def gagnant(self, case_placee: Case) -> bool:
+    def gagnant(self, case_placee: Case, joueur: Union[str, None] = None) -> bool:
         """Calculer si la position actuelle est gagnante
 
         Args:
             case_placee (Case): dernière case placée pour optimiser l'algorithme
+            joueur (Union[str, None], optional): Joueur. Defaults to None.
 
         Returns:
             bool: True si gagnant, sinon False
         """
         fil, col = case_placee
-        cases_joueur = self.coords_joueur[self.joueur_actuel]
+        joueur = joueur or self.joueur_actuel
+        cases_joueur = self.coords_joueur[joueur]
         if self.get_nombre_cases_occupees_du_joueur(self.joueur_actuel) < self.dimension:
             return False
         # check file
@@ -329,13 +333,103 @@ class Morpion():
         return self.res_stack.pop()
 
     def min_max(self) -> tuple[Case, ...]:
-        self.min_max_rec(self.min_max_profondeur)
+        coords_joueur_copy = deepcopy(self.coords_joueur)
+        cases_vides_copy = [*self.cases_vides]
+        plateau, _ = self.min_max_rec(
+            (self.coords_joueur, self.cases_vides, (0, 0), None),
+            self.min_max_profondeur, self.joueur_actuel, 'max'
+        )
+        self.coords_joueur = coords_joueur_copy
+        self.cases_vides = cases_vides_copy
+        if isinstance(plateau[3], tuple):
+            return (plateau[2], plateau[3])
+        return (plateau[2],)
 
-    def min_max_rec(self, k: int):
-        pass
+    def min_max_rec(self, plateau: Plateau, k: int, joueur_a_jouer: str, mode: str
+                    ) -> tuple[Plateau, int]:
+        autre = 'croix' if joueur_a_jouer == 'rond' else 'rond'
+        # set vars pour pouvoir utiliser les méthodes de la classe
+        self.coords_joueur = plateau[0]
+        self.cases_vides = plateau[1]
+        derniere_case_placee = plateau[2]
 
-    def gagnants_possibles(self):
-        pass
+        # si on est à la profondeur max ou l'autre joueur gagne, on renvoie le plateau + son score
+        if k != self.min_max_profondeur:
+            if k == 0 or self.gagnant(derniere_case_placee, self.joueur_actuel):
+                score = self.valeur_min_max(self.joueur_actuel)
+                return plateau, score
+
+        min_score = +math.inf
+        max_score = -math.inf
+
+        # trouver successeurs
+        liste_plateaux_successeurs: list[Plateau] = []
+        cases_vides_avant = [*self.cases_vides]
+        if self.joueur_peut_ajouter_nouvelle_forme(joueur_a_jouer):
+            for case_vide in cases_vides_avant:
+                self.marquer_case(case_vide, joueur_a_jouer)
+                new_coords_joueur = deepcopy(self.coords_joueur)
+                new_cases_vides = [*self.cases_vides]
+                liste_plateaux_successeurs.append(
+                    (new_coords_joueur, new_cases_vides, case_vide, None))
+                self.liberer_case(case_vide, joueur_a_jouer)
+        else:
+            for case_occupee in [*self.coords_joueur[joueur_a_jouer]]:
+                self.liberer_case(case_occupee, joueur_a_jouer)
+                for case_vide in cases_vides_avant:
+                    self.marquer_case(case_vide, joueur_a_jouer)
+                    new_coords_joueur = deepcopy(self.coords_joueur)
+                    new_cases_vides = [*self.cases_vides]
+                    liste_plateaux_successeurs.append(
+                        (new_coords_joueur, new_cases_vides, case_vide, case_occupee))
+                    self.liberer_case(case_vide, joueur_a_jouer)
+                self.marquer_case(case_occupee, joueur_a_jouer)
+
+        best_plateaux = []
+        best_score = 0
+        new_mode = 'min' if mode == 'max' else 'max'
+        # tester tous les plateaux successeurs
+        for plateau_successeur in liste_plateaux_successeurs:
+            plateau_successeur_2, score = self.min_max_rec(
+                plateau_successeur, k - 1, autre, new_mode)
+            if mode == 'max':
+                if score == max_score:
+                    best_plateaux.append(plateau_successeur_2)
+                elif score > max_score:
+                    best_score = score
+                    best_plateaux = [plateau_successeur_2]
+                    max_score = best_score
+            else:
+                if score == max_score:
+                    best_plateaux.append(plateau_successeur_2)
+                if score < min_score:
+                    best_score = score
+                    best_plateaux = [plateau_successeur_2]
+                    min_score = best_score
+        if k == self.min_max_profondeur:
+            return choice(best_plateaux), best_score
+        return plateau, best_score
+
+    def valeur_min_max(self, joueur) -> int:
+        autre = 'croix' if joueur == 'rond' else 'rond'
+        value = self.gagnants_possibles(joueur) - self.gagnants_possibles(autre)
+        self.print_matrice()
+        print('CALCULATED VALUE FOR ' + joueur + ': ' + str(value))
+        return value
+
+    def gagnants_possibles(self, joueur: str) -> int:
+        compteur = 0
+        autre = 'croix' if joueur == 'rond' else 'rond'
+        for k in range(self.dimension):
+            if self.nombre_pions_ligne((k, 0))[autre] == 0:
+                compteur += 1
+            if self.nombre_pions_colonne((0, k))[autre] == 0:
+                compteur += 1
+        if self.nombre_pions_diag_1((0, 0))[autre] == 0:
+            compteur += 1
+        if self.nombre_pions_diag_2((0, self.dimension - 1))[autre] == 0:
+            compteur += 1
+        return compteur
 
     def best_first(self) -> tuple[Case, ...]:
         """IA: calculer pion a placer (ou enlever) par best-first
@@ -418,9 +512,9 @@ class Morpion():
         compteur = {'croix': 0, 'rond': 0}
         i = case[0]
         for j in range(self.dimension):
-            if (i, j) in self.coords_joueur['croix']:
+            if self.case_est_de_joueur((i, j), 'croix'):
                 compteur['croix'] += 1
-            elif (i, j) in self.coords_joueur['rond']:
+            elif self.case_est_de_joueur((i, j), 'rond'):
                 compteur['rond'] += 1
         return compteur
 
@@ -436,9 +530,9 @@ class Morpion():
         compteur = {'croix': 0, 'rond': 0}
         j = case[1]
         for i in range(self.dimension):
-            if (i, j) in self.coords_joueur['croix']:
+            if self.case_est_de_joueur((i, j), 'croix'):
                 compteur['croix'] += 1
-            elif (i, j) in self.coords_joueur['rond']:
+            elif self.case_est_de_joueur((i, j), 'rond'):
                 compteur['rond'] += 1
         return compteur
 
@@ -456,9 +550,9 @@ class Morpion():
         min_coord = min(i, j)
         fil, col = i - min_coord, j - min_coord
         for _ in range(self.dimension - fil - col):
-            if (fil, col) in self.coords_joueur['croix']:
+            if self.case_est_de_joueur((fil, col), 'croix'):
                 compteur['croix'] += 1
-            elif (fil, col) in self.coords_joueur['rond']:
+            elif self.case_est_de_joueur((fil, col), 'rond'):
                 compteur['rond'] += 1
             fil += 1
             col += 1
@@ -478,9 +572,9 @@ class Morpion():
         min_desp = min(self.dimension - j - 1, i)
         fil, col = i - min_desp, j + min_desp
         for _ in range(min(self.dimension - fil, col + 1)):
-            if (fil, col) in self.coords_joueur['croix']:
+            if self.case_est_de_joueur((fil, col), 'croix'):
                 compteur['croix'] += 1
-            elif (fil, col) in self.coords_joueur['rond']:
+            elif self.case_est_de_joueur((fil, col), 'rond'):
                 compteur['rond'] += 1
             fil += 1
             col -= 1
@@ -576,9 +670,9 @@ class Morpion():
             symboles = []
             for j in range(self.dimension):
                 symbole = ' '
-                if (i, j) in self.coords_joueur['croix']:
+                if self.case_est_de_joueur((i, j), 'croix'):
                     symbole = 'X'
-                elif (i, j) in self.coords_joueur['rond']:
+                elif self.case_est_de_joueur((i, j), 'rond'):
                     symbole = 'O'
                 symboles.append(symbole)
             self.log(*symboles, sep=' ', end='')
--- BE/morpion/utils.py
+++ BE/morpion/utils.py
@@ -1,2 +1,6 @@
+from typing import Union
 
 Case = tuple[int, int]
+
+# (dict_cases_joueurs, liste_cases_vides, dernière_case_placée, dernière case enlevée)
+Plateau = tuple[dict[str, list[Case]], list[Case], Case, Union[Case, None]]
