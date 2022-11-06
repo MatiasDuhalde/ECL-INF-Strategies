1a1ba90e57c34675f5586630b12fa9ba8b01e3ef
Matias Duhalde <matias.duhalde@uc.cl>
2022-11-06 02:03:42 +0100

    fix: correct interface comments

--- BE/morpion/morpion_v3_interface.py
+++ BE/morpion/morpion_v3_interface.py
@@ -8,11 +8,11 @@ from morpion_v3 import Morpion
 if __name__ == '__main__':
     DIM = 3
     # humain vs humain
-    # interface = Interface(DIM)
+    # interface = Interface(Morpion, DIM)
 
     # humain vs ia
     interface = Interface(Morpion, DIM, 'ia')
 
     # ia vs ia
-    # interface = Interface(DIM, 'ia', 'ia')
+    # interface = Interface(Morpion, DIM, 'ia', 'ia')
     interface.commencer()
